10428809aa593d34a6602d7f2c6bb088b60537d7
sabilafiridas <sabilafirdias@gmail.com>
2024-03-19 10:22:40 +0700

    Add project files.

---
+++ perulangan/perulangan.cpp
@@ -0,0 +1,13 @@
+// perulangan.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    int arr[5];
+
+}
+
